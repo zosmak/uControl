0a8a9d3d52f93694b919b857098f0b02efbb00f1
joelccarvalho <joelccarvalho>
2016-12-08 21:10:20 +0000

    Mudança de php

--- Base dados e PHP/listar_alarmes.php
+++ Base dados e PHP/listar_alarmes.php
@@ -1,12 +1,11 @@
 <?php
 include 'database.php';
-$pDatabase = DatabasegetInstance();
-$sql = SELECT * FROM alarme;
+$pDatabase = Database::getInstance();
+$sql = "SELECT * FROM alarme";
 $result = $pDatabase->query($sql);
 $rows = array();
 while($temp = mysqli_fetch_assoc($result)) {
     $rows[] = $temp;
 }
 echo json_encode($rows);
-
 ?>
--- Base dados e PHP/listar_dispositivos.php
+++ Base dados e PHP/listar_dispositivos.php
@@ -1,13 +1,11 @@
 <?php
 include 'database.php';
-$pDatabase = DatabasegetInstance();
-$sql = SELECT * FROM dispositivo;
-
+$pDatabase = Database::getInstance();
+$sql = "SELECT * FROM dispositivo";
 $result = $pDatabase->query($sql);
 $rows = array();
 while($temp = mysqli_fetch_assoc($result)) {
     $rows[] = $temp;
 }
 echo json_encode($rows);
-
 ?>
