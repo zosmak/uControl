b4af27194e385b7104a7b6e71520448bfdead060
joelccarvalho <joelccarvalho>
2016-12-15 11:57:16 +0000

    3 layouts feitos, mudei o logo, mudei o nome das páginas no titulo

--- UControlStudio/app/src/main/java/ucontrol/ucontrolstudio/MainActivity.java
+++ UControlStudio/app/src/main/java/ucontrol/ucontrolstudio/MainActivity.java
@@ -1,14 +1,29 @@
 package ucontrol.ucontrolstudio;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView ilumination;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ilumination  = (ImageView)findViewById(R.id.main_illumination);
+
+        ilumination.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), activity_illumination.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
---
+++ UControlStudio/app/src/main/java/ucontrol/ucontrolstudio/activity_ac_schedule.java
@@ -0,0 +1,13 @@
+package ucontrol.ucontrolstudio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_ac_schedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ac_schedule);
+    }
+}
