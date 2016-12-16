052cee503cb3cc9d2df804142bb8785abad61c7a
joelccarvalho <joelccarvalho>
2016-12-16 12:20:27 +0000

    Intents quase todos feitos, cores e ajustes

--- UControlStudio/app/src/main/java/ucontrol/ucontrolstudio/MainActivity.java
+++ UControlStudio/app/src/main/java/ucontrol/ucontrolstudio/MainActivity.java
@@ -8,19 +8,67 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView ilumination;
+    private ImageView illumination, television, air_conditioner, audio, blinds, alarms, doors, manage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ilumination  = (ImageView)findViewById(R.id.main_illumination);
+        illumination  = (ImageView)findViewById(R.id.main_illumination);
+        television  = (ImageView)findViewById(R.id.main_tv);
+        air_conditioner  = (ImageView)findViewById(R.id.main_ac);
+        audio  = (ImageView)findViewById(R.id.main_audio);
+        blinds  = (ImageView)findViewById(R.id.main_blinds);
+        alarms  = (ImageView)findViewById(R.id.main_alarm);
+        doors  = (ImageView)findViewById(R.id.main_doors);
+        manage  = (ImageView)findViewById(R.id.main_manage);
 
-        ilumination.setOnClickListener(new View.OnClickListener() {
+        // Intent para a pag. illumination
+        illumination.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), activity_illumination.class);
+                startActivity(intent);
+            }
+        });
+        // Intent para a pag. ac
+        air_conditioner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), activity_air_conditioner.class);
+                startActivity(intent);
+            }
+        });
+        // Intent para a pag. alarms
+        alarms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), activity_alarms.class);
+                startActivity(intent);
+            }
+        });
+        // Intent para a pag. blinds
+        blinds.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), activity_blinds.class);
+                startActivity(intent);
+            }
+        });
+        // Intent para a pag. doors
+        doors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), activity_doors.class);
+                startActivity(intent);
+            }
+        });
+        // Intent para a pag. manage
+        manage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), activity_manage.class);
                 startActivity(intent);
             }
         });
--- UControlStudio/app/src/main/java/ucontrol/ucontrolstudio/activity_manage.java
+++ UControlStudio/app/src/main/java/ucontrol/ucontrolstudio/activity_manage.java
@@ -1,13 +1,37 @@
 package ucontrol.ucontrolstudio;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class activity_manage extends AppCompatActivity {
+
+    private ImageView add, remove;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage);
+
+        add = (ImageView)findViewById(R.id.imageButtonAdd);
+        remove = (ImageView)findViewById(R.id.imageButtonRemove);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), activity_madd.class);
+                startActivity(intent);
+            }
+        });
+
+        remove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), activity_mremove.class);
+                startActivity(intent);
+            }
+        });
     }
 }
