06822c3608e0d47f9a81e2ca7019b48557250c6b
joelccarvalho <joelccarvalho>
2016-12-22 16:20:11 +0000

    Mudança de php´s

--- Base dados e PHP/inserir_alarme.php
+++ Base dados e PHP/inserir_alarme.php
@@ -6,7 +6,7 @@ $pDatabase = Database::getInstance();
 
 if (isset($_GET['idAlarme']) && isset($_GET['divisao']) && isset($_GET['estado']) && isset($_GET['sensor']))
 {
-$sql = "INSERT INTO alarme (idAlarme, divisao, estado, sensor VALUES ('".$_GET['idAlarme']."','".$_GET['divisao']."','".$_GET['estado']."','".$_GET['sensor']."');";
+$sql = "INSERT INTO alarme (idAlarme, divisao, estado, sensor) VALUES ('".$_GET['idAlarme']."','".$_GET['divisao']."','".$_GET['estado']."','".$_GET['sensor']."');";
 $pDatabase->query($sql);
 $outp ="Inserted";
 }
