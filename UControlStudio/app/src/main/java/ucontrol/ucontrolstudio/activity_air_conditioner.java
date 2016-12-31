0f4f64711b9e500d4d433349a8097a1ab93e84db
joelccarvalho <joelccarvalho>
2016-12-31 11:42:47 +0000

    menu superior

--- UControlStudio/app/src/main/java/ucontrol/ucontrolstudio/activity_air_conditioner.java
+++ UControlStudio/app/src/main/java/ucontrol/ucontrolstudio/activity_air_conditioner.java
@@ -5,6 +5,7 @@ import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -40,18 +41,49 @@ public class activity_air_conditioner extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-
-        MenuItem item = menu.findItem(R.id.spinner);
-        Spinner spinner = (Spinner) MenuItemCompat.getActionView(item);
-
-
-
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.spinner_list_item_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        spinner.setAdapter(adapter);
+        MenuInflater inflater = this.getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
         return true;
+    }
+
+    // opções do canto sup. direito
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.action_tv){
+            Intent intent = new Intent(this, activity_television.class);
+            this.startActivityForResult(intent, 1);
+        }
+        else if(id == R.id.action_illumination){
+            Intent intent = new Intent(this,activity_illumination.class);
+            this.startActivity(intent);
+        }
+        else if(id == R.id.action_ac) {
+            Intent intent = new Intent(this, activity_air_conditioner.class);
+            this.startActivity(intent);
+        }
+        else if(id == R.id.action_audio) {
+            Intent intent = new Intent(this, activity_audio.class);
+            this.startActivity(intent);
+        }
+        else if(id == R.id.action_blinds) {
+            Intent intent = new Intent(this, activity_blinds.class);
+            this.startActivity(intent);
+        }
+        else if(id == R.id.action_alarm) {
+            Intent intent = new Intent(this, activity_alarms.class);
+            this.startActivity(intent);
+        }
+        else if(id == R.id.action_doors) {
+            Intent intent = new Intent(this, activity_doors.class);
+            this.startActivity(intent);
+        }
+        else if(id == R.id.action_manage) {
+            Intent intent = new Intent(this, activity_manage.class);
+            this.startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
